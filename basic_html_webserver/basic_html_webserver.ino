ef64aec1c07daaf11ae2a404c9feb4d3ce10759c
Dan Adrian Mirea <damirea84@gmail.com>
2025-05-22 01:53:34 +0300

    add basic html webserver

--- basic_html_webserver/basic_html_webserver.ino
+++ basic_html_webserver/basic_html_webserver.ino
@@ -1,26 +1,22 @@
 #include <WiFi.h>
 
-// Replace with your network credentials
-const char* ssid     = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
+// Replace these with your Wi-Fi credentials
+const char* ssid = "your_network";
+const char* password = "your_password";
 
 WiFiServer server(80);
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  
-  // Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
 
+  Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("");
+  Serial.println();
   Serial.println("WiFi connected.");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -29,43 +25,32 @@ void setup() {
 }
 
 void loop() {
-  WiFiClient client = server.available();   // listen for incoming clients
-
+  WiFiClient client = server.available();
   if (client) {
     Serial.println("New Client.");
     String currentLine = "";
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        currentLine += c;
-
-        // Check for end of the HTTP request
-        if (c == '\n' && currentLine.length() == 1) {
-          // Send response
+        Serial.write(c);
+        // Read HTTP request but discard content
+        if (c == '\n' && currentLine.length() == 0) {
+          // Send HTTP response
           client.println("HTTP/1.1 200 OK");
           client.println("Content-type:text/html");
+          client.println("Connection: close");
           client.println();
-
-          client.println("<!DOCTYPE html><html>");
-          client.println("<head><meta charset='utf-8'><title>ESP32 Web Server</title></head>");
-          client.println("<body><h1>Hello from ESP32!</h1>");
-          client.print("<p>Uptime: ");
-          client.print(millis() / 1000);
-          client.println(" seconds</p>");
-          client.println("</body></html>");
-
-          client.println();
+          client.println("<!DOCTYPE html><html><body><h1>Hello from ESP32!</h1></body></html>");
           break;
-        }
-
-        if (c == '\n') {
+        } else if (c != '\r') {
+          currentLine += c;
+        } else {
           currentLine = "";
         }
       }
     }
-
     delay(1);
     client.stop();
-    Serial.println("Client Disconnected.");
+    Serial.println("Client disconnected.");
   }
 }
