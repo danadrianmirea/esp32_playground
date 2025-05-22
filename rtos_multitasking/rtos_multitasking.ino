a4e9fd4c55bc979b5282fbb991ed06cd727a355e
Adrian Mirea <152083571+danadrianmirea@users.noreply.github.com>
2025-05-22 15:55:21 +0300

    Update rtos_multitasking.ino

--- rtos_multitasking/rtos_multitasking.ino
+++ rtos_multitasking/rtos_multitasking.ino
@@ -1,71 +1,23 @@
-#include <WiFi.h>
+void Task1(void *pvParameters) {
+  while (true) {
+    Serial.println("Task 1 is running");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
 
-// Replace with your network credentials
-const char* ssid     = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
-
-WiFiServer server(80);
+void Task2(void *pvParameters) {
+  while (true) {
+    Serial.println("Task 2 is running");
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  
-  // Connect to Wi-Fi
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  server.begin();
+  xTaskCreatePinnedToCore(Task1, "Task1", 1000, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(Task2, "Task2", 1000, NULL, 1, NULL, 1);
 }
 
 void loop() {
-  WiFiClient client = server.available();   // listen for incoming clients
-
-  if (client) {
-    Serial.println("New Client.");
-    String currentLine = "";
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        currentLine += c;
-
-        // Check for end of the HTTP request
-        if (c == '\n' && currentLine.length() == 1) {
-          // Send response
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-type:text/html");
-          client.println();
-
-          client.println("<!DOCTYPE html><html>");
-          client.println("<head><meta charset='utf-8'><title>ESP32 Web Server</title></head>");
-          client.println("<body><h1>Hello from ESP32!</h1>");
-          client.print("<p>Uptime: ");
-          client.print(millis() / 1000);
-          client.println(" seconds</p>");
-          client.println("</body></html>");
-
-          client.println();
-          break;
-        }
-
-        if (c == '\n') {
-          currentLine = "";
-        }
-      }
-    }
-
-    delay(1);
-    client.stop();
-    Serial.println("Client Disconnected.");
-  }
+  // nothing here
 }
