96ca7584271171bb10e8175dd4d9a01a7a6fc483
Dan Adrian Mirea <damirea84@gmail.com>
2025-05-29 19:25:37 +0300

    set correct frequency

--- pwm_example_30p_duty/pwm_example_30p_duty.ino
+++ pwm_example_30p_duty/pwm_example_30p_duty.ino
@@ -1,5 +1,5 @@
 const int pwmPin = 15;       // digital output pin 15
-const int pwmFreq = 5;       // 5hz so the led blinking is visible
+const int pwmFreq = 300;     // 300hz
 const int pwmResolution = 8; // 8 bit resolution (0-255)
 
 void setup() {
@@ -7,7 +7,7 @@ void setup() {
   ledcAttach(pwmPin, pwmFreq, pwmResolution);
 
   // Set duty cycle (0-255 for 8-bit resolution)
-  ledcWrite(pwmPin, 128);  // ~50% duty cycle
+  ledcWrite(pwmPin, 77);  // ~50% duty cycle
 }
 
 void loop() {
